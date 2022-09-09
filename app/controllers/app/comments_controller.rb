44f20bd8ec8f4a54b1caf2d93f8d45b8ab858815
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-09 22:43:36 +0430

    add the method controller action for creating comments in response json

--- app/controllers/app/comments_controller.rb
+++ app/controllers/app/comments_controller.rb
@@ -11,4 +11,14 @@ class Api::CommentsController < ApplicationController
             render json: @comments, status: :ok
         end
     end
+
+    def create
+        @comment = Comment.new(text: params[:text], user_id: @decoded['id'], post_id: params[:post_id])
+
+        if @comment.save
+            render json: @comment, status: :created
+        else
+            render json: { error: "Comment not created" }, status: :unprocessable_entity
+        end
+    end
 end

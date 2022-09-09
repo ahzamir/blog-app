fff70bdc2bfe6c76d68c7d531ddbbe7af9cf4ad6
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-09 22:52:37 +0430

    add routes for the api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,15 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # API routes
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
 end
